ea8f79d781c0cb10e65d34dea8bec6467c9f5635
jaimemin <jaimemin@gmail.com>
2021-12-30 23:51:44 +0900

    seperated pointcut to different class in order to use globally

---
+++ src/main/java/com/tistory/jaimemin/aop/order/aop/Pointcuts.java
@@ -0,0 +1,16 @@
+package com.tistory.jaimemin.aop.order.aop;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class Pointcuts {
+
+    @Pointcut("execution(* com.tistory.jaimemin.aop.order..*(..))")
+    public void allOrder() {} // pointcut signature
+
+    // 클래스 이름 패턴이 *Service
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService() {}
+
+    @Pointcut("allOrder() && allService()")
+    public void orderAndService() {}
+}
